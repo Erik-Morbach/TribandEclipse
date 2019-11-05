1b1c6f9ea2f53c655f415b11e74845a359710ca3
Erik Morbach <morbacherik@gmail.com>
2019-11-04 23:37:21 -0300

    Ordenacoes adicionadas: por preco e por proximidade.

--- src/main/java/com/integrador/Triband/TribandApplication.java
+++ src/main/java/com/integrador/Triband/TribandApplication.java
@@ -11,49 +11,51 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.integrador.model.AvaliacaoBanda;
 import com.integrador.model.Banda;
 import com.integrador.model.EntidadeBase;
+import com.integrador.model.Estudio;
 import com.integrador.model.Foto;
 import com.integrador.model.Reserva;
 import com.integrador.persistencia.BandaDAO;
+import com.integrador.persistencia.EstudioDAO;
 import com.integrador.persistencia.FotoDAO;
+import com.integrador.persistencia.GenericoDAO;
 import com.integrador.persistencia.Parser;
 
 @SpringBootApplication
 public class TribandApplication {
 
 	public static void main(String[] args) {
-		
 
-		BandaDAO dao = new BandaDAO();
+		EstudioDAO dao = new EstudioDAO();
 
-		Banda a = dao.buscarPorEmail("a@g.com");
-		System.out.println(a.getId());
-		System.out.println(a.getNome());
-		System.out.println(a.getIntegrantes());
-		System.out.println(a.getEmail());
-		System.out.println(a.getSenha());
-	/*;
-		dao.deletar(1);
-	FotoDAO fotoDAO = new FotoDAO(new Foto());
-		
-		Foto foto = fotoDAO.salvar(new Foto((long) 0,"teste3",null,null));
-		
-		AgendaDAO agendaDAO= new AgendaDAO(new Agenda());
-		
-		Agenda agenda = agendaDAO.salvar(new Agenda((long)0,null,null,new ArrayList<Reserva>()));
-		
-		Banda a = new Banda((long) 0,"jaja","sdf@gmail.com","1234",3,foto,agenda,new ArrayList<Foto>(),new ArrayList<AvaliacaoBanda>());
-		
-
-		a = dao.salvar(a);
-		
-		agenda.setBanda(a);
-		foto.setBanda(a);
-		
-		agendaDAO.editar(agenda);
-		fotoDAO.editar(foto);
-		dao.editar(a);
-		*/
-		//SpringApplication.run(TribandApplication.class, args);
+		List<Estudio> b = dao.buscaPorProximidadeLocalizacaoPrecoHorarioDisponivel(-28.449293, -52.199451, (double) 3, null, null, null, null, EstudioDAO.ORDENAPORPRECO);
+		for (Estudio a : b) {
+			System.out.println(a.getId());
+			System.out.println(a.getNome());
+			System.out.println(a.getDescricao());
+			System.out.println(a.getEmail());
+			System.out.println(a.getSenha());
+		}
+		/*
+		 * ; dao.deletar(1); FotoDAO fotoDAO = new FotoDAO(new Foto());
+		 * 
+		 * Foto foto = fotoDAO.salvar(new Foto((long) 0,"teste3",null,null));
+		 * 
+		 * AgendaDAO agendaDAO= new AgendaDAO(new Agenda());
+		 * 
+		 * Agenda agenda = agendaDAO.salvar(new Agenda((long)0,null,null,new
+		 * ArrayList<Reserva>()));
+		 * 
+		 * Banda a = new Banda((long) 0,"jaja","sdf@gmail.com","1234",3,foto,agenda,new
+		 * ArrayList<Foto>(),new ArrayList<AvaliacaoBanda>());
+		 * 
+		 * 
+		 * a = dao.salvar(a);
+		 * 
+		 * agenda.setBanda(a); foto.setBanda(a);
+		 * 
+		 * agendaDAO.editar(agenda); fotoDAO.editar(foto); dao.editar(a);
+		 */
+		// SpringApplication.run(TribandApplication.class, args);
 	}
 
 }
--- src/main/java/com/integrador/persistencia/EstudioDAO.java
+++ src/main/java/com/integrador/persistencia/EstudioDAO.java
@@ -3,6 +3,7 @@ package com.integrador.persistencia;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 import org.springframework.data.util.Pair;
 
@@ -10,70 +11,128 @@ import com.integrador.model.Estudio;
 import com.integrador.model.Localizacao;
 
 public class EstudioDAO extends GenericoDAO<Estudio> {
+	public static final int ORDENAPORPRECO = 1;
+	public static final int ORDENAPORPROXIMIDADE = 2;
 
 	public EstudioDAO() {
 		super(new Estudio());
 		// TODO Auto-generated constructor stub
 	}
+
 	public Estudio buscarPorEmail(String email) {
-		return buscaUm("email",email);
+		return buscaUm("email", email);
 	}
-	public List<Estudio> buscarPorProximidade(Double latitude,Double longitude,Double raio){
-		return busca("WHERE distanciaEntrePontos(?,?,localizacao.latitude,localizacao.longitude)<=? ", new Object[] {latitude,longitude,raio});
+
+	public List<Estudio> buscarPorProximidade(Double latitude, Double longitude, Double raio) {
+		return busca(
+				"WHERE distancia(?,?,localizacao.latitude,localizacao.longitude)<=? ORDER BY distancia(?,?,localizacao.latitude,localizacao.longitude)",
+				new Object[] { latitude, longitude, raio, latitude, longitude });
 	}
-	public List<Estudio> buscaPorLocalizacao(Localizacao localizacao){
-		return buscaPorAtributo("localizacao",localizacao);
+
+	public List<Estudio> buscaPorLocalizacao(Localizacao localizacao) {
+		return buscaPorAtributo("localizacao", localizacao);
 	}
-	public List<Estudio> buscaPorPreco(Double preco){
-		return busca("WHERE estudio.preco<=?",preco);
+
+	public List<Estudio> buscaPorPreco(Double preco) {
+		return busca("WHERE estudio.preco<=?", preco);
 	}
-	public List<Estudio> buscaPorHorarioDisponivel(Time inicio, Time fim){
+
+	public List<Estudio> buscaPorHorarioDisponivel(Time inicio, Time fim) {
 		return null;
 	}
-	public List<Estudio> buscaPorProximidadeLocalizacaoPrecoHorarioDisponivel(Double latitude,Double longitude,Double raio,Localizacao localizacao,Double preco,Time inicio,Time fim){
-		
-		String queryProximidade = "distanciaEntrePontos(?,?,localizacao.latitude,localizacao.longitude)<=?";
+
+	public List<Estudio> buscaPorProximidadeLocalizacaoPrecoHorarioDisponivel(Double latitude, Double longitude,
+			Double raio, Localizacao localizacao, Double preco, Time inicio, Time fim, int ordenacao) {
+
+		String queryProximidade = "distancia(?,?,localizacao.latitude,localizacao.longitude)<=?";
 		ArrayList<Object> valorProximidade = new ArrayList<Object>();
-		valorProximidade.add(latitude); valorProximidade.add(longitude); valorProximidade.add(raio);
-		
-		Pair<String,List<Object>> queryLocalizacaoPair =  geraQuery("localizacao",localizacao,"",null);
+		valorProximidade.add(latitude);
+		valorProximidade.add(longitude);
+		valorProximidade.add(raio);
+
+		Pair<String, List<Object>> queryLocalizacaoPair = 
+				(localizacao != null
+				?
+				geraQuery("localizacao", localizacao, "", null)
+				: 
+				Pair.of("", new ArrayList<Object>()));
+
 		String queryLocalizacao = queryLocalizacaoPair.getFirst();
 		ArrayList<Object> valorLocalizacao = (ArrayList<Object>) queryLocalizacaoPair.getSecond();
 		String queryPreco = "estudio.preco<=?";
 		ArrayList<Object> valorPreco = new ArrayList<Object>();
 		valorPreco.add(preco);
-		String queryHorarioDisponivel="";
+		String queryHorarioDisponivel = "";
 		ArrayList<Object> valorHorarioDisponivel = new ArrayList<Object>();
-		valorHorarioDisponivel.add(inicio);valorHorarioDisponivel.add(fim);
-		
-		String querys[] = {queryProximidade,queryLocalizacao,queryPreco,queryHorarioDisponivel};
-		ArrayList<ArrayList<Object>> valorQuerys = new ArrayList<ArrayList<Object>>(); 
+		valorHorarioDisponivel.add(inicio);
+		valorHorarioDisponivel.add(fim);
+
+		String querys[] = { queryProximidade, queryLocalizacao, queryPreco, queryHorarioDisponivel };
+		ArrayList<ArrayList<Object>> valorQuerys = new ArrayList<ArrayList<Object>>();
 		// java não possui arrayList<Object> [], então temos que usar um
-		//ArrayList<ArrayList<Object>>
-		valorQuerys.add(valorProximidade); valorQuerys.add(valorLocalizacao);
-		valorQuerys.add(valorPreco); valorQuerys.add(valorHorarioDisponivel);
-		
-		String query = " WHERE";
+		// ArrayList<ArrayList<Object>>
+		valorQuerys.add(valorProximidade);
+		valorQuerys.add(valorLocalizacao);
+		valorQuerys.add(valorPreco);
+		valorQuerys.add(valorHorarioDisponivel);
+
+		String query = "";
+		String ordena = "";
 		ArrayList<Object> valores = new ArrayList<Object>();
-		int foi = 0;
-		for(int i=0;i<querys.length;i++) {
+
+		for (int i = 0; i < querys.length; i++) {
 
 			int tenta = 0;
-			for(int j=0;j<valorQuerys.get(i).size();j++) {
-				if(valorQuerys.get(i).get(j)==null) tenta++;
+			for (int j = 0; j < valorQuerys.get(i).size(); j++) {
+				if (valorQuerys.get(i).get(j) == null)
+					tenta++;
 			}
-			
-			if(tenta==valorQuerys.get(i).size()) continue;
-			
-			foi++;
-			query+= " "+querys[i];
-			valores.add(valorQuerys.get(i));
+
+			if (tenta == valorQuerys.get(i).size())
+				continue; // se todos os valores da query forem nulos você pula pra proxima;
+
+			query += " " + querys[i];
+
+			valorQuerys.get(i).forEach(new Consumer<Object>() {
+				public void accept(Object t) {
+					valores.add(t);
+				}
+			});
+
 		}
-		if(foi==0) query = "";
+		if (!query.isEmpty())
+			query = " WHERE ".concat(query);
+
+		Pair<String, List<Object>> ordenacaoPair = geraOrdenacao(ordenacao, latitude, longitude);
+		ordena = ordenacaoPair.getFirst();
 		
-		return busca(query,valores);
+		ordenacaoPair.getSecond().forEach(new Consumer<Object>() {
+			public void accept(Object t) {
+				valores.add(t);
+			}
+		});
+		query += ordena;
+
+		return busca(query, valores.toArray());
 	}
-	
-	
-	
+
+	private Pair<String, List<Object>> geraOrdenacao(int ordenacao, double latitude, double longitude) {
+		String ordena = "";
+		ArrayList<Object> valores = new ArrayList<Object>();
+		switch (ordenacao) {
+		case ORDENAPORPRECO:
+			ordena += "estudio.preco";
+			break;
+		case ORDENAPORPROXIMIDADE:
+			ordena += "distancia(?,?,localizacao.latitude,localizacao.longitude)";
+			valores.add(latitude);
+			valores.add(longitude);
+			break;
+		default:
+			break;
+		}
+		if (!ordena.isEmpty())
+			ordena = " ORDER BY ".concat(ordena);
+		return Pair.of(ordena, valores);
+	}
 }

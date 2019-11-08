db9daedd581ed5c38ca7159b4d4af9b8217279df
alunoinfo <alunoinfo@localhost>
2019-11-08 12:17:12 -0200

    Anotações adicionadas, Dao Generico devera ser modificado

--- src/main/java/com/integrador/model/Atributo.java
+++ src/main/java/com/integrador/model/Atributo.java
@@ -1,5 +1,12 @@
 package com.integrador.model;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Atributo {
 	String nome();
 	Class<?> tipo();
